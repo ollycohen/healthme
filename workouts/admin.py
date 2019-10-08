b3f0951c9251b38e15c5c9c9fad0c3479a589217
= <jsangiamo@wustl.edu>
2019-10-08 15:18:29 -0500

    added one to one relationships to workout models

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Workout, Cardio
 # Register your models here.
+
+
+# Registering these models to be tested within admin console
+admin.site.register(Workout)
+admin.site.register(Cardio)
--- workouts/models.py
+++ workouts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Workout(models.Model):
     exercise_name = models.CharField(max_length = 50, help_text="The name of the workout you did.")
@@ -6,6 +7,9 @@ class Workout(models.Model):
     reps = models.PositiveSmallIntegerField(help_text="The number of reps you attempted for each set.")
     success = models.BooleanField(help_text="Did you hit your sets and reps?")
     date = models.DateTimeField(help_text="Date of exercise.")
+
+    # associate each workout with exactly one user
+    user = models.OneToOneField(User)
 
     def __str__(self):
        return self.exercise_name + " for " + self.sets + " sets of " + self.reps + " was succesful: " + self.success + " on " + self.date
@@ -23,6 +27,9 @@ class Cardio(models.Model):
     duration = models.PositiveSmallIntegerField(help_text="How long you performed this cardio for.")
     distance_in_miles= models.DecimalField(max_digits=2, decimal_places=2, help_text="Distance traveled.")
     date = models.DateTimeField(help_text="Date of exercise.")
+
+    # associate each workout with exactly one user
+    user = models.OneToOneField(User)
 
     def __str(self):
         return self.type + " for " + self.duration + " went " + self.distance_in_miles + " miles"  + self.date
