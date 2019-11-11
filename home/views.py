9489e7e43e538a41cc2ee322dea0b5b8aad8c6f3
= <jsangiamo@wustl.edu>
2019-11-11 12:55:52 -0600

    fixed bug with home page not requiring users to be logged in

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from datetime import timezone
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/auth/signup')
 def home(request):
     return render(request, 'home/home.html', {})
