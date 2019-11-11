ebd85e4b4dd8bb665ba6ca731f703a535b6325b1
= <jsangiamo@wustl.edu>
2019-11-11 15:47:05 -0600

    fixed messages, added basic insights (total workouts, workout success ratio

--- workouts/views.py
+++ workouts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from datetime import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import WorkoutForm, CardioForm
 from .models import Cardio, Workout
@@ -10,25 +11,25 @@ from .models import Cardio, Workout
 def add_workout(request):
     context = {'workoutForm': WorkoutForm(), 'cardioForm': CardioForm()}
     if request.method == "POST":
-        print(request.POST)
         if 'workout' in request.POST:
-            print("--- WORKOUT DECTECTED ---")
             form = WorkoutForm(request.POST)
             if form.is_valid():
                 workout = form.save(commit=False)
                 workout.user = request.user
                 workout.save()
+                messages.success(request, 'Your workout was recorded succesfully!')
+            else:
+                messages.error(request, "There was an error recording your workout!")
         if 'cardio' in request.POST:
-            print("--- CARDIO DECTECTED ---")
             form = CardioForm(request.POST)
             if form.is_valid():
-                print("VALID CARDIO")
                 cardio = form.save(commit=False)
                 cardio.user = request.user
                 cardio.save()
+                messages.success(request, 'Your cardio was recorded succesfully!')
             else:
-                print("INVALID CARDIO")
-
+                messages.error(request, "There was an error recording your cardio!")
+        return render(request, 'workouts/addWorkout.html', context)
     return render(request, 'workouts/addWorkout.html', context)
 
 @login_required(login_url='/auth/signup')
