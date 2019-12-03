9dc93e3248020945f31cefac8b162f18a4d5577c
= <jsangiamo@wustl.edu>
2019-12-02 20:06:22 -0600

    added footer

--- workouts/models.py
+++ workouts/models.py
@@ -56,3 +56,8 @@ class Weight(models.Model):
 
     class Meta:
         ordering = ['date']
+
+
+# class Exercise(models.Model):
+#     exercise_name = models.CharField(max_length=50)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
